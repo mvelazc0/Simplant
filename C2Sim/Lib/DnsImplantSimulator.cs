7ff799f68ebf0e6aae3bfaa9ed4aec08572626d2
mvelazco <you@example.com>
2021-03-20 01:07:43 -0400

    First commit

---
+++ C2Sim/Lib/DnsImplantSimulator.cs
@@ -0,0 +1,95 @@
+﻿using DnsClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+
+namespace C2Sim.Lib
+{
+
+    public class DnsImplantSimulator
+    {
+
+        public string domain { get; set; } = "getbobspizza.com";
+        public string[] subdomains { get; set; } = { "c2", "pizza", "deepdish", "resolver" };
+        public static int sleep { get; set; } = 2;
+        public int port { get; set; } = 53;
+        public static Lib.Logger logger { get; set; }
+
+        public DnsImplantSimulator()
+        {
+            logger = new Lib.Logger(AppDomain.CurrentDomain.BaseDirectory + "beaconsim.log");
+            logger.StartDnsBeacon();
+        }
+
+        public static string RandomString(int length)
+        {
+            Random random = new Random();
+            string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            string rstring= new string(Enumerable.Repeat(chars, length).Select(s => s[random.Next(s.Length)]).ToArray());
+            return rstring;
+            //var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(rstring);
+            //return System.Convert.ToBase64String(plainTextBytes);
+
+        }
+
+        public static bool TxtDnsResolution(string fqdn)
+        {
+            try
+            {
+                logger.TimestampInfo(String.Format("Resolving TXT record for ", fqdn));
+                IPHostEntry hostInfo = Dns.GetHostEntry(fqdn);
+                return true;
+            }
+            
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static void TxtResolution(string fqdn)
+        {
+            logger.TimestampInfo(String.Format("Resolving TXT record for {0}", fqdn));
+            var lookup = new LookupClient();
+
+            try
+            {
+                var result = lookup.Query(fqdn, QueryType.TXT);
+                if (result.Answers.Count > 1)
+                {
+                    foreach (var TxtRecord in result.Answers.TxtRecords())
+                    {
+                        logger.TimestampInfo(TxtRecord.ToString());
+                    }
+                }
+                else 
+                {
+                    logger.TimestampInfo("No result found");
+                }
+            }
+            catch 
+            {
+                logger.TimestampInfo("Error with DNS request");
+
+            }
+
+        }
+
+        public void run()
+        {
+            while (true)
+            {
+                Random rnd = new Random();
+                Thread.Sleep(sleep * 1000);
+                TxtResolution(RandomString(63)+"."+ subdomains[rnd.Next(1, 4)] + "."+domain);
+            }
+        }
+
+    }
+
+
+
+}
---
+++ C2Sim/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace C2Sim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lib.DnsImplantSimulator dnsim = new Lib.DnsImplantSimulator();
+            dnsim.run();
+        }
+    }
+}
